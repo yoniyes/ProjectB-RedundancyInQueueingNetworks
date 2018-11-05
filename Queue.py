1bceedaf233ced5e55e3f49c7b389aa27fc06a24
Yonatan Yeshanov <yoniyes@campus.technion.ac.il>
2018-11-05 14:57:44 +0200

    basic classes - Queue & QueueNetwork - for sim.

---
+++ Queue.py
@@ -0,0 +1,67 @@
+class Queue:
+    """This class describes a queue in a network running in discrete time with the ability to add and
+    remove workload."""
+
+    ##
+    # Given a service and an initial workload, initializes a Queue.
+    # Both parameters are integers where service > 0 and workload >= 0.
+    ##
+    def __init__(self, service=1, workload=0):
+        if int(service) <= 0 or int(workload) < 0:
+            raise Exception("Illegal arguments for Queue __init__")
+        self.service = int(service)
+        self.workload = int(workload)
+        self.timePassed = 0
+
+    ##
+    # Resets the queue to service and workload of 0.
+    ##
+    def reset(self, service=1):
+        self.setService(service)
+        self.workload = 0
+        self.timePassed = 0
+
+    ##
+    # Returns the current workload.
+    ##
+    def getWorkload(self):
+        return self.workload
+
+    ##
+    # Sets the workload to the given size. workload is an integer and workload >= 0.
+    ##
+    def setWorkload(self, workload):
+        if int(workload) < 0:
+            raise Exception("Illegal arguments for Queue setWorkload")
+        self.workload = int(workload)
+
+    ##
+    # Adds the given workload to the queue. Makes sure the total workload is >= 0.
+    # Notice: this method can be used to decrease the workload if passing a negative value.
+    ##
+    def addWorkload(self, workload):
+        if self.workload + int(workload) < 0:
+            self.workload = 0
+        else:
+            self.workload += int(workload)
+
+    ##
+    # Returns the current service.
+    ##
+    def getService(self):
+        return self.service
+
+    ##
+    # Sets the service to the given size. service is an integer and it is > 0.
+    ##
+    def setService(self, service):
+        if int(service) <= 0:
+            raise Exception("Illegal arguments for Queue setService")
+        self.service = int(service)
+
+    ##
+    # Reduces the amount of workload in Queue by service and increments the time that passed.
+    ##
+    def advanceTimeSlot(self):
+        self.addWorkload(-self.getService())
+        self.timePassed += 1
---
+++ QueueNetwork.py
@@ -0,0 +1,118 @@
+from Queue import Queue
+import numpy as np
+
+
+class QueueNetwork:
+    """Class for operating a queue network in discrete time."""
+
+    ##
+    # Initialize a queue network given a size, service rates and initial workloads.
+    # If only a size was given, all queues will have service rate = 1, workload = 0.
+    # All parameters are integers where size > 0, all service rates > 0 and workloads >= 0.
+    ##
+    def __init__(self, size, services=[], workloads=[]):
+        _size = int(size)
+        _services = services
+        _workloads = workloads
+        if _size < 1:
+            raise Exception("Illegal size for QueueNetwork")
+        for i in range(len(_services)):
+            _services[i] = int(_services[i])
+            if _services[i] < 1:
+                raise Exception("Illegal service rates for QueueNetwork")
+        for i in range(len(_workloads)):
+            _workloads[i] = int(_workloads[i])
+            if _workloads[i] < 0:
+                raise Exception("Illegal workloads for QueueNetwork")
+        if not _services:
+            _services = [1 for i in range(_size)]
+        if not _workloads:
+            _workloads = [0 for i in range(_size)]
+
+        # Member fields init.
+        self.queues = [Queue(_services[i], _workloads[i]) for i in range(_size)]
+        self.size = _size
+
+    ##
+    # Resets all queues to services and workload of 0.
+    ##
+    def reset(self, services=[]):
+        _services = services
+        if len(_services) == 0:
+            _services = [1 for i in range(self.getSize())]
+        if len(_services) != self.getSize():
+            raise Exception("Services vector size mismatch with queue network size while trying to reset")
+        [self.queues[i].reset(_services[i]) for i in range(self.getSize())]
+
+    ##
+    # Returns a list of the current services.
+    ##
+    def getServices(self):
+        services = [q.getService() for q in self.queues]
+        return services
+
+    ##
+    # Returns a list of the current workloads.
+    ##
+    def getWorkloads(self):
+        workloads = [q.getWorkload() for q in self.queues]
+        return workloads
+
+    ##
+    # Returns the total workload in the network.
+    ##
+    def getTotalWorkload(self):
+        return np.sum(self.getWorkloads())
+
+    ##
+    # Returns the number of queues in the network.
+    ##
+    def getSize(self):
+        return self.size
+
+    ##
+    # Sets the services to the given sizes. services are integers and are > 0.
+    ##
+    def setServices(self, services=[]):
+        if not services or len(services) != self.size:
+            raise Exception("Illegal number of service rates for setServices")
+        for i in range(self.size):
+            if int(services[i]) < 1:
+                raise Exception("Illegal service rates for setServices")
+        [self.queues[i].setService(int(services[i])) for i in range(self.size)]
+
+    ##
+    # Sets the workloads to the given sizes. Workloads are integers and are >= 0.
+    ##
+    def setWorkloads(self, workloads=[]):
+        if not workloads or len(workloads) != self.size:
+            raise Exception("Illegal number of workloads for setWorkloads")
+        for i in range(self.size):
+            if int(workloads[i]) < 0:
+                raise Exception("Illegal workloads for setWorkloads")
+        [self.queues[i].setWorkload(int(workloads[i])) for i in range(self.size)]
+
+    ##
+    # Adds the given workloads to the specified queues.
+    # If no index is given, workloads size must be self.getSize() and will be added to all queues accordingly.
+    # Notice: this method can be used to decrease the workload if passing a negative value.
+    ##
+    def addWorkload(self, workloads, index=[]):
+        _index = []
+        if len(index) == 0:
+            _index = range(self.getSize())
+        else:
+            for i in range(len(index)):
+                if int(index[i]) < 0 or int(index[i]) > self.size - 1:
+                    raise Exception("Illegal index for addWorkload")
+                _index.append(int(index[i]))
+        _workloads = [int(workload) for workload in workloads]
+        if len(_workloads) != len(_index):
+            raise Exception("Length of workloads doesn't match length of indices in addWorkload")
+        [self.queues[_index[i]].addWorkload(_workloads[i]) for i in range(len(_index))]
+
+    ##
+    # Reduces the amount of workload in every queue by its service rate and increments the time that passed.
+    ##
+    def advanceTimeSlot(self):
+        [q.advanceTimeSlot() for q in self.queues]
